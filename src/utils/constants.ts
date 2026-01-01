57b9b4a441b87553d87d20b4028e599e836454ee
opaque-maniac <mbithimark8@gmail.com>
2026-01-01 16:51:29 +0300

    Fixed ts issue

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,6 @@ import type { NovaConfigs } from "./types";
 export const defaultConfigs: NovaConfigs = {
   logActivity: true,
   maxBodySize: 10 * 1024 * 1024,
-  staticPath: "public",
 };
 
 export const FILE_MEMORY_THRESHOLD = 1024 * 1024; // 1MB
